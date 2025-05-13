6ede37ddea5b6afbec3ce7ae182efb8c807a2e4a
thebeast01 <mohammadsaif0847@gmail.com>
2025-05-13 15:36:54 +0530

    fixed

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import dotenv from 'dotenv'
-
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
 import authRoute from './routes/authRoute';
 dotenv.config()
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: ["*", "http://localhost:3000"],
-  credentials: true,
+  origin: ["http://localhost:3000", "https://vibetrails.vercel.app"],
+  credentials: true
 }))
 app.get("/", (req, res) => {
-  res.send("Server Working")
+  res.send("Hello World")
 })
-app.use("/api/auth", authRoute)
+app.use('/api/auth', authRoute)
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port `)
+  console.log(`Server is running on port ${process.env.PORT}`);
 })
