961f89711b5220fc1ec2d32e5eaf7f18540bb164
thebeast01 <mohammadsaif0847@gmail.com>
2025-05-13 15:21:06 +0530

    Updated

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -159,11 +159,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://neondb_owner:npg_z78VrBUgKpLA@ep-lingering-salad-a16cmpxx-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+        "value": null
       }
     }
   },
--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -2,5 +2,4 @@ import { Router } from "express";
 const authRoute = Router();
 import { signup } from "../controllers/auth/authControllers";
 authRoute.get("/signup", signup);
-
 export default authRoute;
