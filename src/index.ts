9d1bcb4fa59d9c2b060771c43aeee810117167c7
thebeast01 <mohammadsaif0847@gmail.com>
2025-05-13 15:33:38 +0530

    Updated index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
 import authRoute from './routes/authRoute';
+dotenv.config()
 const app = express();
-app.use(cors({
-  credentials: true,
-}))
 app.use(express.json())
 app.use(cookieParser())
-const port = process.env.PORT || 8000
+app.use(cors({
+  origin: ["*", "http://localhost:3000"],
+  credentials: true,
+}))
 app.get("/", (req, res) => {
   res.send("Server Working")
 })
 app.use("/api/auth", authRoute)
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port `)
 })
