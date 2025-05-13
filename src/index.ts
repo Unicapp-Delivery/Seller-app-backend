190d81e0286c67b4d4deaf2a0eb3210c3e13058f
thebeast01 <mohammadsaif0847@gmail.com>
2025-05-13 14:52:32 +0530

    Added Env

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import cors from 'cors'
-
 const app = express();
 const port = process.env.PORT || 8000
-
 app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 app.use(express.json())
-
+app.use(cookieParser())
 app.get("/", (req, res) => {
   res.send("Server Working")
 })
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
